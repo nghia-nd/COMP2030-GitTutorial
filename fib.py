b52cca6f92c03761283d731e10c9e1e96277c4b7
khointn <nguyentietnguyenkhoi@gmail.com>
2022-11-02 10:02:33 +0700

    fix error zeroth fib

--- fib.py
+++ fib.py
@@ -7,5 +7,8 @@ Negative numbers should return None
 def fibonacci(position):
   if(position == 1 or position == 2):
     return 1
+    
+   if position == 0:
+   return 0
   return fibonacci(position - 1) + fibonacci(position - 2)
   
